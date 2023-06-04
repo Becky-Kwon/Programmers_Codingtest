fa8ea28018c7285d8e76165753e03ba391420423
Becky_kwon <59564626+Becky-Kwon@users.noreply.github.com>
2023-06-05 03:05:39 +0900

    Update [해시] 전화번호 목록.py

--- 코딩테스트 고득점 Kit/[해시] 전화번호 목록.py
+++ 코딩테스트 고득점 Kit/[해시] 전화번호 목록.py
@@ -1,3 +1,6 @@
+# https://velog.io/@chaegil15/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%95%B4%EC%8B%9C-%EC%A0%84%ED%99%94%EB%B2%88%ED%98%B8-%EB%AA%A9%EB%A1%9D
+
+
 def solution(phone_book):
     answer = True
     phone_book.sort()
